a16dda70064d8e4a5e9d090fd61fe4f79b5d7878
Dana <danayim@bu.edu>
2026-07-29 20:03:57 +0900

    pushing Sum D2

---
+++ src/dana/m07/swea/Sum.java
@@ -0,0 +1,29 @@
+package dana.m07.swea;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Sum
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+				 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			 */
+			/////////////////////////////////////////////////////////////////////////////////////////////
+
+		}
+	}
+}
