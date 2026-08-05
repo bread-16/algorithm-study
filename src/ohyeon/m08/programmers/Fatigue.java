ddb8df56f7487646460a5f049dc3c8ea5a89388e
Sirius147 <kwonohyeon1114@gmail.com>
2026-08-06 08:42:16 +0900

    feat: programmers 피로도 수정

--- src/ohyeon/m08/programmers/Fatigue.java
+++ src/ohyeon/m08/programmers/Fatigue.java
@@ -1,5 +1,30 @@
 package ohyeon.m08.programmers;
 
 public class Fatigue {
+	int maxCount = 0; 
+    boolean[] visited;
 
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        dfs(k, 0, dungeons);
+        
+        return maxCount;
+    }
+
+    public void dfs(int k, int count, int[][] dungeons) {
+        maxCount = Math.max(maxCount, count);
+
+        if (count == dungeons.length) {
+            return;
+        }
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], count + 1, dungeons);
+                visited[i] = false;
+            }
+        }
+    }
 }
