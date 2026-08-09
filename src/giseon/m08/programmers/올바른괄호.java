f77f5733c94038f2b175779f4d652a9a80e1d569
gitseon <namkisunjang@gmail.com>
2026-08-09 14:56:16 +0900

    sovle 올바른 괄호

---
+++ src/giseon/m08/programmers/올바른괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+// 0.02ms, 82.3MB
+class 올바른괄호 {
+    boolean solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        // stack이 비어있으면 넣으셈
+        // 비어있지 않다면 짝 비교
+        // peek()가 '(' 일때 ')'를 만나면 pop
+        // 스택이 비어있다면 true, 비어있지 않다면 false
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == '(' && s.charAt(i) == ')') {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
