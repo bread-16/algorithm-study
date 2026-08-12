7ef28102fc167f85c09179c44db8a897e84846f9
ohyeon-dev <kwonohyeon1114@gmail.com>
2026-08-12 09:39:29 +0900

    feat: programmers 타겟넘버 문제풀이

---
+++ src/ohyeon/m08/programmers/TargetNum.java
@@ -0,0 +1,26 @@
+package ohyeon.m08.programmers;
+
+public class TargetNum {
+	int[] numbers;
+    int target;
+    int answer;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    
+    void dfs (int idx, int sum) {
+        if(idx == numbers.length) {
+            if(sum == target) answer++;
+            return;
+        }
+        
+        dfs(idx + 1, sum + numbers[idx]);
+        dfs(idx + 1, sum - numbers[idx]);
+    }
+}
